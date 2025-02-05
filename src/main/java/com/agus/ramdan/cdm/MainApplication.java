a3d2d211470c39555c1da71463bdaacc083dd33a
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-02-06 00:56:50 +0700

    exclude database config

--- src/main/java/com/agus/ramdan/cdm/MainApplication.java
+++ src/main/java/com/agus/ramdan/cdm/MainApplication.java
@@ -2,10 +2,14 @@ package com.agus.ramdan.cdm;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 public class MainApplication {
 
     public static void main(String[] args) {
