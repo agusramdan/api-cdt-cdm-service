c2ff09c4a80487deb5f9f3028a92ebbc19a37d60
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-02-09 15:21:26 +0700

    Validation Token

---
+++ src/main/java/com/agus/ramdan/cdm/utils/ValidTokenQRValidator.java
@@ -0,0 +1,34 @@
+package com.agus.ramdan.cdm.utils;
+
+
+import com.agus.ramdan.cdm.exception.ResourceNotFoundException;
+import com.agus.ramdan.cdm.service.BpmsClient;
+//import jakarta.validation.ConstraintValidator;
+//import jakarta.validation.ConstraintValidatorContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+@Component
+public class ValidTokenQRValidator implements ConstraintValidator<ValidTokenQR, String> {
+
+    @Autowired
+    private BpmsClient client; // Service yang dipanggil
+
+    @Override
+    public boolean isValid(String merchantId, ConstraintValidatorContext context) {
+        if (merchantId == null || merchantId.isEmpty()) {
+            return false; // Merchant ID tidak boleh kosong
+        }
+        try {
+            client.validateCode(merchantId);
+            return true; // Panggil service untuk validasi
+        }catch (ResourceNotFoundException e){
+            return false;
+        }
+
+
+    }
+}
