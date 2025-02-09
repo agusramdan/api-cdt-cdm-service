a76d1c9cbfa79c050f060a7f1e29c073be482c76
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-02-09 16:30:05 +0700

    Feigh Changes interceptor

--- src/main/java/com/agus/ramdan/cdm/interceptor/AuthorizationRequestInterceptor.java
+++ src/main/java/com/agus/ramdan/cdm/interceptor/AuthorizationRequestInterceptor.java
@@ -1,15 +1,13 @@
 package com.agus.ramdan.cdm.interceptor;
 import feign.RequestInterceptor;
 import feign.RequestTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
-@Component
-public class FeignClientInterceptor implements RequestInterceptor {
+public class AuthorizationRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate requestTemplate) {
