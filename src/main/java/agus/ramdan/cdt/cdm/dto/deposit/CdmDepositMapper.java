2c63d95d16a10a0de65ae1661070a8161c4c92c9
Agus Muhammad Ramdan <agus.ramdan@gmail.com>
2025-03-15 17:58:20 +0700

    Update CDM validation fixed

--- src/main/java/agus/ramdan/cdt/cdm/dto/deposit/CdmDepositMapper.java
+++ src/main/java/agus/ramdan/cdt/cdm/dto/deposit/CdmDepositMapper.java
@@ -1,10 +1,11 @@
 package agus.ramdan.cdt.cdm.dto.deposit;
 
 import agus.ramdan.cdt.core.trx.controller.dto.deposit.TrxDepositCreateDTO;
+import agus.ramdan.cdt.core.trx.controller.dto.deposit.TrxDepositDenQueryDTO;
 import agus.ramdan.cdt.core.trx.controller.dto.deposit.TrxDepositQueryDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
+
+import java.util.List;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface CdmDepositMapper {
@@ -15,5 +16,13 @@ public interface CdmDepositMapper {
 
     @Mapping(target = "terminalId", source = "machine.code")
     @Mapping(target = "branch",source = "machineInfo")
+    @Mapping(target = "denominations", source = "denominations", qualifiedByName = "mapListToCdmDepositDenDTO")
     CdmDepositDTO toTrxDepositDTO(TrxDepositQueryDTO request);
+
+    @Named("mapToCdmDepositDenDTO")
+    CdmDepositDenDTO mapToCdmDepositDenDTO(TrxDepositDenQueryDTO entity);
+
+    @IterableMapping(qualifiedByName = "mapToCdmDepositDenDTO")
+    @Named("mapListToCdmDepositDenDTO")
+    List<CdmDepositDenDTO> mapListToCdmDepositDenDTO(List<TrxDepositDenQueryDTO> entities);
 }
